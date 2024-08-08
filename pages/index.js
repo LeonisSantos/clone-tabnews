f69c6836197d3660ad355aa170e43bd8eebc51ce
LeonisSantos <81047279+LeonisSantos@users.noreply.github.com>
2024-08-08 19:09:24 +0000

    adiciona a página inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home(){
+    return <h1>"Eis que estou à porta e bato. Se alguém ouvir a minha voz e abrir a porta, entrarei e cearei com ele, e ele comigo." Apocalipse 3-20</h1>
+}
+
+export default Home;
